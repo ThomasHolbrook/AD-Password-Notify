45873b490fd6568ff0ae25bc7ecc95ab689f7f8f
Tom <tom@tjh.local>
2017-12-06 15:39:16 +0000

    Initial Commit

---
+++ ADPassPrompt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ADPassPrompt
+//
+//  Created by Thomas Holbrook on 06/12/2017.
+//  Copyright © 2017 Thomas Holbrook. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
